3d5d76afafb1663f2a524bc0f7f3d4e05193fb38
Bombay <72895600+BombayV@users.noreply.github.com>
2021-08-30 15:14:04 -0400

    refactor(server/sql): implemented db into event

--- server/server.lua
+++ server/server.lua
@@ -15,33 +15,26 @@ RegisterNetEvent('ev:getVehicle', function(price, type, model, plate, properties
     local source <const> = source
     local xPlayer = ESX.GetPlayerFromId(source)
     if xPlayer then
-        local xMoney = xPlayer.getAccount(type).money
-        if xMoney > tonumber(price) then
-            setVehicle(plate, properties)
-            xPlayer.removeAccountMoney(type, tonumber(price))
-            xPlayer.showNotification('Paid $' .. price .. '. Money left $' .. xMoney - tonumber(price))
-        else
-            xPlayer.showNotification('You need ' .. tostring('$' .. tonumber(price) - xMoney))
-        end
+        MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE plate = @plate', {
+            ['@plate'] = plate
+        }, function(result)
+            if result[1] == nil then
+                local xMoney = xPlayer.getAccount(type).money
+                if xMoney > tonumber(price) then
+                    MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle) VALUES (@owner, @plate, @vehicle)', {
+                        ['@owner']   = xPlayer.getIdentifier(),
+                        ['@plate']   = plate,
+                        ['@vehicle'] = json.encode(properties)
+                    }, function(result)
+                        xPlayer.removeAccountMoney(type, tonumber(price))
+                        xPlayer.showNotification('Paid $' .. price .. '. Money left $' .. xMoney - tonumber(price) .. '\nGo check it out at your garage')
+                    end)
+                else
+                    xPlayer.showNotification('You need ' .. tostring('$' .. tonumber(price) - xMoney))
+                end
+            else
+                xPlayer.showNotification('The plate already exists. Try again or change the plate')
+            end
+        end)
     end
 end)
-
-function setVehicle(plate, properties)
-    local source <const> = source
-    local xPlayer = ESX.GetPlayerFromId(source)
-    MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE plate = @plate', {
-        ['@plate'] = plate
-    }, function(result)
-        if result[1] == nil then
-            MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle) VALUES (@owner, @plate, @vehicle)', {
-                ['@owner']   = xPlayer.getIdentifier(),
-                ['@plate']   = plate,
-                ['@vehicle'] = json.encode(properties)
-            }, function(result)
-                print("veh urs")
-            end)
-        else
-            print('Plate repeats')
-        end
-    end)
-end
